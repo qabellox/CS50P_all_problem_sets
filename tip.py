2b45bb88686beab94b8e4c7936ed6214b49f7eb8
qabellox <adnankabeel2010@gmail.com>
2026-07-11 12:27:12 +0300

    Delete tip.py

--- tip.py
+++
@@ -1,4 +0,0 @@
-cost = int(input("How much was the meal? ").replace("$", ""))
-percentage = int(input("What percentage would you like to tip? ").replace("%", "")) / 100
-tip = cost * percentage
-print(f"Leave ${tip:.2f}")
